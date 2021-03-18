cb436aa6c825836867b89190558af9fef301cffc
tiavlovsky <45367923+tiavlovsky@users.noreply.github.com>
2021-03-18 16:05:21 +0000

    Update analysis_utils.py

--- sandbox/meio/gsm/analysis_utils.py
+++ sandbox/meio/gsm/analysis_utils.py
@@ -6,24 +6,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from sandbox.meio.gsm.convert_excel_to_csv import parse_willems_graphs, write_to_csv, \
+from sandbox.meio.gsm.convert_excel_to_csv import parse_supply_chain_graphs, write_to_csv, \
     load_into_excel
 from meio.experiment.gsm_experiment_utils import run_gsm_with_timeout, plot_gsm
 from meio.gsm.utils import GSM, read_supply_chain_from_txt
 
 
 def get_network(supply_chain):
-    willems_file = "../../../meio/willems_dataset/data/MSOM-06-038-R2 Data Set in Excel.xls"
-    data_xls = load_into_excel(willems_file)
+    sup_chain_file = "../../../meio/supply_chain_dataset/data/..."
+    data_xls = load_into_excel(sup_chain_file)
     no_supply = "{:02d}".format(supply_chain)
-    stage_configs, up_stages, network_description = parse_willems_graphs(data_xls, no_supply)
+    stage_configs, up_stages, network_description = parse_supply_chain_graphs(data_xls, no_supply)
     return network_description, stage_configs, up_stages
 
 
-def get_reported_costs(willems_dir: str):
-    # Optimal cost from DAG paper
-    #   -> paper http://willems.utk.edu/papers/dl/Humair_Willems_OR_v59n3_MayJun2011.pdf
-    with open(os.path.join(willems_dir, "reported_optimal_cost.txt")) as f:
+def get_reported_costs(supply_chain_dir: str):
+    with open(os.path.join(supply_chain_dir, "reported_optimal_cost.txt")) as f:
         reported_costs_data = f.readlines()
         reported_costs = {i + 1: int(x.strip()) for i, x in enumerate(reported_costs_data)}
     return reported_costs
@@ -32,14 +30,14 @@ def get_reported_costs(willems_dir: str):
 def run_gsm(supply_chain: int, timeout: int):
     """
     Run gsm experiment
-    :param supply_chain: which willems supply to run on
+    :param supply_chain: which supply chain to run on
     :param timeout: interrupt execution after so many seconds
     :return: nothing. Output is stored json file
     """
     """thread worker function"""
-    willems_dir = '../../../meio/willems_dataset/data/'
+    supply_chain_dir = '../../../meio/supply_chain_dataset/data/'
     network_description, stage_configs, up_stages = get_network(supply_chain)
-    supply_chain_root = 'willems_dataset/exps/temp{}'.format(supply_chain)
+    supply_chain_root = 'supply_chain_dataset/exps/temp{}'.format(supply_chain)
     # generate temporary csv file that gets loaded back in - needed in run_gsm_with_timeout
     write_to_csv(supply_chain_root + '.csv', stage_configs, up_stages)
     print(supply_chain_root + '.csv', network_description)
@@ -53,7 +51,7 @@ def run_gsm(supply_chain: int, timeout: int):
         with open(supply_chain_root+'.json', 'r') as fp:
             results = json.load(fp)
 
-        print_supply_chain_log(supply_chain, results, willems_dir, supply_chain_root,
+        print_supply_chain_log(supply_chain, results, supply_chain_dir, supply_chain_root,
                                network_description, stage_configs, up_stages)
         print(supply_chain, 'finished gsm')
 
@@ -61,15 +59,14 @@ def run_gsm(supply_chain: int, timeout: int):
         print(supply_chain, 'has broken down lead times.')
 
 
-def print_supply_chain_log(supply_chain: int, results: Optional[Dict], willems_dir: str,
+def print_supply_chain_log(supply_chain: int, results: Optional[Dict], supply_chain_dir: str,
                            supply_chain_root: str, network_description: Dict,
                            stage_configs: Dict, up_stages: Dict, plot: bool = True):
     if results is None:
         print('No results for supply chain {} timeout may have occured'.format(supply_chain))
         return
-    # Stock levels from stochastic leads paper
-    #   -> paper http://willems.utk.edu/papers/dl/Humair_et_al_IF_v43n5_SepOct2013.pdf
-    reported_stock = pd.read_csv('../../../meio/willems_dataset/data/optimal_inventory_levels.csv')
+
+    reported_stock = pd.read_csv('../../../meio/supply_chain_dataset/data/optimal_inventory_levels.csv')
     ref_stock_row = reported_stock[(reported_stock.SupplyChain == supply_chain) &
                                    (reported_stock.LT == 'mean')]
     reported_safety_stock, reported_base_stock = np.nan, np.nan
@@ -78,7 +75,7 @@ def print_supply_chain_log(supply_chain: int, results: Optional[Dict], willems_d
         reported_safety_stock = int(ref_stock_row.SafetyStock.values)
         reported_base_stock = int(ref_stock_row.TotalStock.values)
 
-    ref_cost = get_reported_costs(willems_dir)[supply_chain]
+    ref_cost = get_reported_costs(supply_chain_dir)[supply_chain]
     base_stock_levels = np.sum(np.fromiter(results['base_stocks'].values(), dtype=int))
     safety_stock_levels = np.sum(np.fromiter(results['safety_stocks'].values(), dtype=int))
     cost = results['solution']['cost']
